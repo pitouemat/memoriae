2c0ac00d7b5e5bfe89ea7e847a29c4dd45a38780
Matheus <79723385+pitouemat@users.noreply.github.com>
2021-09-08 15:23:01 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Memoriae</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="content">
+  
+    <div class="main">
+
+   
+
+    <div class="btn">
+
+    <h1>Memoriae</h1>
+    <p>Compartilhe Momentos</p>
+        <button><a href="share.php"> Compartilhar</a></button>
+        <button> <a href="photo.php"> Reviver</a></button>
+    </div>
+    <div class="image-cover">
+        <img src="img/photo.svg" alt="" width="400">
+    </div>
+    
+    </div>
+    </div>
+    
+</body>
+</html>
